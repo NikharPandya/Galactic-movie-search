3e21b79d81c3110982eef81c00b6a9325c1dd959
NikharPandya <pandyanikhar@gmail.com>
2023-05-05 15:00:24 -0400

    defined func for handling and changing data when clicked

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const MovieList: React.FC = () => {
   return (
-    <ul>
-      <li key={1}>Movie title </li>
-    </ul>
+    <>
+      <div className="container mx-auto w-screen flex justify-center">
+        <ul className="w-3/4">
+          <li
+            className="h-10 bg-slate-200 p-2 text-slate-700 hover:text-slate-400"
+            key={1}>
+            Movie title
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { useAppDispatch } from "../store";
+import { searchMoviesStart } from "../store";
 
 const SearchForm: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState<string>(" ");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(searchMoviesStart(searchQuery));
+  };
+
+  const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="h-40 w-screen p-6">
-      <form className="flex justify-center gap-4">
+      <form onSubmit={handleSubmit} className="flex justify-center gap-4">
         <input
           className="input input-bordered w-full max-w-xs"
           type="text"
           placeholder="Search for a Star Wars Movie"
+          value={searchQuery}
+          onChange={onHandleChange}
         />
         <button type="submit" className="btn gap-2">
           <MagnifyingGlassIcon className="h-6 w-6" />
